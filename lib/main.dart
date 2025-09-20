c4879e4d008ab0687f6f81fd480499bb361addd9
525UmerYousaf <umer.yousaf.587@gmail.com>
2025-09-20 18:01:56 +0500

    Passing State between Screens using Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
 void main() {
-  //  ProviderScope is resposible for storing states of providers
-  //  which user want to share among the widgets.
-  runApp(ProviderScope(child: const MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
       home: const MyHomePage(),
     );
@@ -36,24 +36,55 @@ class MyHomePage extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Riverpod Starter"),
+        title: const Text("Riverpod app"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text('$count', style: Theme.of(context).textTheme.headlineMedium),
+            Text('${count}', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(appCount.notifier).state++;
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => SecondPage(),
+                ),
+              );
+            },
+            heroTag: 'one',
+            tooltip: 'Increment',
+            child: const Icon(Icons.arrow_right_rounded),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              ref.read(appCount.notifier).state++;
+            },
+            heroTag: 'two',
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SecondPage extends ConsumerWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    int count = ref.watch(appCount);
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text("$count", style: TextStyle(fontSize: 30))),
     );
   }
 }
